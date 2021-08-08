8025a511a9d083ee91ba1e158582ab2a55e9221d
Radu Baltag <baltag.radu@gmail.com>
2021-08-08 10:02:02 +0200

    Display Closed Tasks functionality

--- src/CloseTasks.js
+++ src/CloseTasks.js
@@ -2,11 +2,21 @@ import React from "react";
 
 class CloseTasks extends React.Component {
   render() {
+    const closeTasks = this.props.tasks.filter(tasks => tasks.done === true);
     return (
       <div className="doneContainer">
         <div className="containerTitle">
         <h3>Closed</h3></div>
-        <div className="tasks"></div>
+        <div className="tasks">
+          <ul>
+            {closeTasks.map((task, index) => {
+                return (
+                  <li key={"task"+index}>{task.title}</li>
+                )
+              })
+            }
+          </ul>
+        </div>
       </div>
     );
   }
