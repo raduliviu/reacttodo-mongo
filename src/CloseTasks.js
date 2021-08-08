34aef2e8fc0643c989ed2f0d2884ecd6e5ed170f
Erik <elaktrato@gmail.com>
2021-08-08 11:10:19 +0200

    Make separate TaskItem Element

--- src/CloseTasks.js
+++ src/CloseTasks.js
@@ -9,23 +9,7 @@ class CloseTasks extends React.Component {
         <div className="containerTitle">
           <h3>Closed</h3></div>
         <div className="tasks">
-          <ul>
-            {closeTasks.map((task, index) => {
-              return (
-                <div class="taskItem done" key={"task" + index} id={"task" + index}>
-                  <label class="container taskCheck">
-                    <input type="checkbox" checked/>
-                    {task.title}
-                    <span class="checkmark"></span>
-                  </ label>
-                  <div class="buttons">
-                    <div type="button" value="Delete" class="icon delete" onClick={"displayModal" + index}></div>
-                  </div>
-                </div>
-              )
-            })
-            }
-          </ul>
+          <TaskItem dividedTasks={closeTasks} />
         </div>
       </div>
     );
