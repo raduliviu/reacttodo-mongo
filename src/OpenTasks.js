8a2cdd1810876fe3b126c9739ba83c9c3d76f21d
Erik <elaktrato@gmail.com>
2021-08-08 10:45:21 +0200

    making adding tasks work

--- src/OpenTasks.js
+++ src/OpenTasks.js
@@ -8,15 +8,32 @@ class OpenTasks extends React.Component {
 
         return ( <div className = "openContainer" >
             <div className = "containerTitle" >
-            <h3 > Open </h3> </div> <div className = "tasks" >
-            <ul >
+              <h3 > 
+                Open 
+              </h3> 
+              <div class="taskForm">
+                <input type="text" id="taskBox" />
+                <div type="button" class="icon add" id="createTask" onClick={this.props.createTask}>
+                </div>
+              </div> 
+            </div> 
+            <div className = "tasks" >
             {openTasks.map((task, index) => {
               return (
-                <li key={"task"+index}>{task.title}</li>
+                <div class="taskItem " key={"task"+index} id={"task"+index}>
+                        <label class="container taskCheck">
+                            <input type="checkbox" />
+                            {task.title}
+                            <span class="checkmark"></span>
+                        </ label>
+                        <div class="buttons">
+                            <div type="button" value="Edit" class="icon edit" onClick={"editTask"+index}></div>
+                            <div type="button" value="Delete" class="icon delete" onClick={"displayModal" + index}></div>
+                    </div>
+                </div>
               )
-            })}
-
-            </ul>
+            })
+            }
 
             </div>
             </div>
