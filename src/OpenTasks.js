28cc0601c73c34407e6cac6f491896d9592a8d77
Erik <elaktrato@gmail.com>
2021-08-08 09:44:38 +0200

    Render tasks

--- src/OpenTasks.js
+++ src/OpenTasks.js
@@ -1,16 +1,27 @@
 import React from "react";
 
+
 class OpenTasks extends React.Component {
-  render() {
-    return (
-      <div className="openContainer">
-        <div className="containerTitle">
-          <h3>Open</h3>
-        </div>
-        <div className="tasks"></div>
-      </div>
-    );
-  }
+
+    render() {
+      const openTasks = this.props.tasks.filter(tasks => tasks.done === false);
+
+        return ( <div className = "openContainer" >
+            <div className = "containerTitle" >
+            <h3 > Open </h3> </div> <div className = "tasks" >
+            <ul >
+            {openTasks.map((task, index) => {
+              return (
+                <li key={"task"+index}>{task.title}</li>
+              )
+            })}
+
+            </ul>
+
+            </div>
+            </div>
+        );
+    }
 }
 
 export default OpenTasks;
--- src/TasksWrapper.js
+++ src/TasksWrapper.js
@@ -4,17 +4,38 @@ import CloseTasks from "./CloseTasks";
 import Stats from "./Stats";
 
 class TasksWrapper extends React.Component {
-  render() {
-    return (
-      <main className="tasksFlex">
-      <div className="tasksWrapper">
-        <OpenTasks />
-        <CloseTasks />
-        </div>
-        <Stats />
-      </main>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            tasks: [
+                {
+                    title: "do something",
+                    done: false
+                },
+                {
+                    title: "anything",
+                    done: false
+                },
+                {
+                    title: "nothing",
+                    done: true
+                }
+              ]
+
+        }
+    }
+    render() {
+        return ( 
+        < main className = "tasksFlex" >
+            <div className = "tasksWrapper" >
+            <OpenTasks tasks={this.state.tasks} />
+            <CloseTasks tasks={this.state.tasks} />
+            </div> 
+            <Stats tasks={this.state.tasks} />
+        </main>
+        );
+    }
 }
 
 export default TasksWrapper;
